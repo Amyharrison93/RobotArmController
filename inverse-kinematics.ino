c249c333101c4ec2ad8e5bb8306b2549fa69a4ad
Amy Harrison <amycantflynow@gmail.com>
2020-10-19 01:23:35 +0100

    slight changes to kinematics

--- inverse-kinematics.ino
+++ inverse-kinematics.ino
@@ -1,9 +1,14 @@
 //inverse kinematics functions
 #include "robot-arm-config.hpp"
 #include <math.h>
+#include <vector>
+#include <memory>
+
+using std::vector;
+using std::unique_ptr;
 
 //calculate the angle for the joint
-float calcTrigAngle(float arm1Length, float arm2Length, float arm3Length, float jointDistance)
+vector<float> calcTrigAngle1(float arm1Length, float arm2Length, float arm3Length, float jointDistance)
 {
     float arm1LengthSqr = pow(arm1Length,2);
     float arm2LengthSqr = pow(arm2Length,2);
@@ -13,13 +18,14 @@ float calcTrigAngle(float arm1Length, float arm2Length, float arm3Length, float 
     float joint2Angle = acos((arm3LengthSqr + arm1LengthSqr - arm2LengthSqr)/2*arm3Length*arm1Length);
     float joint3Angle = acos((arm1LengthSqr + arm2LengthSqr - arm3LengthSqr)/2*arm1Length*arm2Length);
 
+    return {joint1Angle, joint2Angle, joint3Angle};
 }
 
 float calcJointDistance(float distance)
 {
-    float jointDistance; 
-
-    jointDistance = distance/armSegments-1;
+    float jointDistance = distance/armSegments-1;
 
     return jointDistance;
 }
+//calculated the distance from the base to the point from x,y,z
+//float calcPointDistance()
